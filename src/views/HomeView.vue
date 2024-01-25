4a2d9b564eb95a76b08048d4152468e6f147a81f
CandussoR <r.candusso@protonmail.com>
2024-01-25 11:45:05 +0100

    feat : update timer to now call

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,27 @@
     </div>
 
     <div>
-        <router-link to="/time-record">New timer</router-link>
+        <router-link to="/new">New timer</router-link>
+    </div>
+    <div>
+        <p v-if="success" id="success" class="success">{{ success }}</p>
+        <p v-if="error" id="error" class="error">{{ error }}</p>
+        <button @click="handleUpdate()">Update last timer to now</button>
     </div>
 </template>
 
 <script setup>
+import axios from 'axios';
+import { ref } from 'vue';
 
+const success = ref('')
+const error = ref('')
+
+function handleUpdate() {
+    axios.put('/time_records/last_to_now')
+        .then(() => success.value = "Updated, boss.")
+        .catch(() => error.value = "An error occured.")
+}
 </script>
 
 <style scoped>
