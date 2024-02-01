ffc42d61e78e4a4fb88e5810e02529b094be295e
CandussoR <r.candusso@protonmail.com>
2024-02-01 12:16:08 +0100

    refactor : moved store toggle responsibility

--- src/components/modals/NewTagModal.vue
+++ src/components/modals/NewTagModal.vue
@@ -1,11 +1,11 @@
 <template>
         <h3>New Tag</h3>
-        <form @submit.prevent="handleSubmit(tag)">
+        <form @submit.prevent="handleSubmit">
             <div>
                 <label for="newTag" hidden="hidden">New Tag</label>
                 <input class="modalInput" id="newTag" type="text" placeholder="tag name" maxlength="30" v-model="tag" required/>
             </div>
-            <p v-if="successMsg" class="success">{{ successMsg }}</p>
+            <p v-if="msg" class="success">{{ msg }}</p>
             <button class="modalButton" type="submit">Create</button> 
         </form>
 </template>
@@ -13,12 +13,13 @@
 <script setup>
 import { useTagStore } from '@/stores/tag';
 import { ref } from 'vue';
+const tagStore = useTagStore()
 const tag = ref(null)
 const msg = ref('')
-const tagStore = useTagStore()
 
-async function handleSubmit(tag) {
-    const res = await tagStore.createTag(tag)
+async function handleSubmit() {
+    tagStore.isCreated = false
+    const res = await tagStore.createTag(tag.value)
     if (res == 200) {
         msg.value = "Tag successfully created"
     }
