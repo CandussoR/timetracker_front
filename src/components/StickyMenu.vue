100cc70174e07bb9a9fb4e1b224ede8e70b9692c
CandussoR <r.candusso@protonmail.com>
2024-03-16 17:27:22 +0100

    feat : aside as burger menu on mobile and column on wider screen

--- src/components/StickyMenu.vue
+++ src/components/StickyMenu.vue
@@ -1,10 +1,11 @@
 <template>
     <aside id="menu">
         <!-- Only show a burger menu for mobile device -->
-        <div id="mobile-menu" v-if="windowWidth < 764" @click="toggleMobileMenu()">
-            <button id="menu" type="button" class="button"><span class="material-symbols-outlined">menu</span> Menu</button>
-        </div>
-        <!-- Show the menu on wide screen or at click of menu -->
+        <button id="menu-button" type="button" class="menu-button" v-if="windowWidth < 764" @click="toggleMobileMenu()">
+            <span class="material-symbols-outlined">menu</span> 
+             Menu
+        </button>
+        <!-- Show the menu on wider screens or at click of burger menu -->
         <div v-if="isToggled || windowWidth > 764" :class="[isExpanded ? 'menu is-expanded' : 'menu']">
             <div id="menu-wrap" class="menu-toggle" v-if="windowWidth > 764">
                 <button id="wrap" type="button" class="wrap" @click="isExpanded = !isExpanded">
@@ -78,129 +79,100 @@ function toggleMobileMenu() {
 </script>
 
 <style scoped>
+    .menu-button {
+        appearance: none;
+        border: none;
+        outline: none;
+        background: none;
+        color: var(--text);
+        display: flex;
+        justify-items: flex-start;
+        align-items: center;
+        font-size: 1.5rem;
+        width: 100vw;
+        padding: 1em;
+    }
     .menu {
-        background-color: var(--background);
+        position: fixed;
         display: flex;
         flex-direction: column;
-        position: fixed;
-        width: fit-content;
+        width: max-content;
+        background-color: var(--background);
         transition: 0.2s ease-out;
         z-index: 99;
-        /* Always keep the menu in the middle of the screen if in column */
-        margin-right: 2em;
-        top: 0; 
-        bottom: 0;
-        left: 0; 
-        right: 0; 
-        height: fit-content;
-        margin: auto 0;
+        height: 100vh;
     }
-    .menu-toggle {
-        display:flex;
-        justify-content: flex-end;
-    }
-
     .menu-item {
-        display: flex; 
-        flex-grow: 1;
-        align-items: center;
-        margin-bottom: 1%;
+            display: flex; 
+            flex-grow: 1;
+            align-items: center;
+            margin-bottom: 1%;
     }
-
     .menu-item p {
-        padding: 0 1rem;
-        color: var(--background);
-        background: var(--primary);
-        font-weight: 500;
-        border-radius: 0 0.3em 0.3em 0;
-        justify-content: center;
-        transform: translateX(-5px);
+            padding: 0 1rem;
+            color: var(--background);
+            background: var(--primary);
+            font-weight: 500;
+            border-radius: 0 0.3em 0.3em 0;
+            justify-content: center;
+            transform: translateX(-5px);
+        }
+    .material-symbols-outlined {
+        font-size: clamp(1.5rem, 1.5rem, 3rem)
     }
 
+    /* Menu item background + color styles */
     .menu-item:hover p, 
     .menu-item:hover .svg
-  {
+    {
         color: var(--text);
         background: var(--accent); 
     }
-
     .menu-item.active p,
     .menu-item.active .svg {
         color: var(--text);
         background: var(--secondary);
     }
-
-
-    /* Whether expand, or unroll, or burger, make it smaller ? */
-    span:first {
-        width: 2rem;
-        height: 2rem;
-    }
-
+    /* Svg + p span style */
     span {
-        display: flex;
-        justify-content: center; /* Center horizontally */
-        align-items: center;
-        width: 3rem;
-        height: 3rem;
-    }
-
-    .material-symbols-outlined {
-        font-size: clamp(1.5rem, 1.5rem, 3rem)
-    }
-
-    .wrap {
-        background-color: var(--expanded);
-        color: var(--text);
-        border-style: none;
-    }
-
-    .is-expanded .wrap {
-        transform: rotate(-180deg);
-    }
-
+            display: flex;
+            justify-content: center; /* Center horizontally */
+            align-items: center; /* Center vertically */
+            width: 3rem;
+            height: 3rem;
+        }
     p {
         width: calc(80% - 2%); /* width without the padding of the p to not deform the svg */
     }
 
-    @media (max-width: 764px) {
-        #menu {
-            display: flex;
-            flex-direction: column;
-        }
+    @media screen and (min-width: 764px) {
         .menu {
-            position: relative;
-            width: fit-content;
-            padding: 0 2em;
+            /* Always keep the menu in the middle of the screen if in column */
+            margin-right: 2em;
+            top: 0; 
+            bottom: 0;
+            left: 0; 
+            right: 0; 
             height: fit-content;
-            background-color: var(--background);
-            margin: 0;
-            margin-top: .3em;
+            margin: auto 0;
         }
-
-        .menu-item {
-            width: fit-content;
+        /* Whether expand, or unroll, or burger, make it smaller ? */
+        span:first {
+            width: 2rem;
+            height: 2rem;
         }
-
-        button {
-            appearance: none;
-            border: none;
-	        outline: none;
-	        background: none;
+        /* Styling the expand arrow */
+        .menu-toggle {
+            display:flex;
+            justify-content: flex-end;
+        }
+        .wrap {
+            background-color: var(--expanded);
             color: var(--text);
-            display: flex;
-            justify-items: flex-start;
-            align-items: center;
-            font-size: 1.5rem;
-            width: 100vw;
-        }
-
-        #mobile-menu {
-            display: flex;
-            justify-items: flex-start;
-            justify-content: space-between;
-            width: 100%;
-            padding: 0.5em;
+            border-style: none;
+        }   
+        .is-expanded .wrap {
+            transform: rotate(-180deg); /* points inward when expanded */
         }
     }
 </style>
