5cdc4d711d6af153e0a1e0c92db415ce47357294
CandussoR <r.candusso@protonmail.com>
2024-03-17 15:45:03 +0100

    feat : added edit button with submenu to sticky menu.

--- src/components/StickyMenu.vue
+++ src/components/StickyMenu.vue
@@ -6,7 +6,9 @@
              Menu
         </button>
         <!-- Show the menu on wider screens or at click of burger menu -->
-        <div v-if="isToggled || windowWidth > 764" :class="[isExpanded ? 'menu is-expanded' : 'menu']">
+        <div v-if="isToggled || windowWidth > 764" 
+            :class="[isExpanded ? 'menu is-expanded' : 'menu']"
+           v-on-click-outside="closeMenu">
             <div id="menu-wrap" class="menu-toggle" v-if="windowWidth > 764">
                 <button id="wrap" type="button" class="wrap" @click="isExpanded = !isExpanded">
                     <span class="material-symbols-outlined">keyboard_double_arrow_right</span>
@@ -14,28 +16,46 @@
             </div>
             <router-link to="/">
                 <div class="menu-item" :class="{ active: route.path === '/' }">
-                    <span class="material-symbols-outlined svg" @mouseover="home = !home" @mouseleave="home = !home">home</span>
+                    <span class="material-symbols-outlined svg">home</span>
                     <p class="primary" v-show="isExpanded">Home</p>
                 </div>
             </router-link>   
             <router-link to="/new" >
                 <div class="menu-item" :class="{ active: route.path === '/new' }">
-                    <span class="material-symbols-outlined svg" @mouseover="clock = !clock" @mouseleave="clock = !clock">alarm</span>
+                    <span class="material-symbols-outlined svg">alarm</span>
                     <p class="primary" v-show="isExpanded">New clock</p>
                 </div>
             </router-link>
             <router-link to="/old">
                 <div class="menu-item" :class="{ active: route.path === '/old' }">
-                    <span class="material-symbols-outlined svg" @mouseover="oldRecord = !oldRecord" @mouseleave="oldRecord = !oldRecord">alarm_add</span>
+                    <span class="material-symbols-outlined svg">alarm_add</span>
                     <p class="primary" v-show="isExpanded">Add Record</p>
                 </div>
             </router-link>
             <router-link to="/time_records">
                 <div class="menu-item" :class="{ active: route.path === '/time_records' }">
-                    <span class="material-symbols-outlined svg" @mouseover="search = !search" @mouseleave="search = !search">search</span>
+                    <span class="material-symbols-outlined svg">search</span>
                     <p class="primary" v-show="isExpanded">New search</p>
                 </div>
             </router-link>
+            <div id="edit-menu-container">
+                <div id="edit-menu">
+                    <div id="edit-menu__header" class="menu-item" 
+                    @click="toggleSubmenu"
+                    :class="{ active: route.path === ('/tasks' || '/tags') }">
+                        <span class="material-symbols-outlined svg">edit</span>
+                        <p class="primary" v-show="isExpanded">Edit</p>
+                    </div>
+                    <div id="edit-menu__submenu" class="submenu" v-if="editSubmenu">
+                        <router-link to="/tasks">
+                            <p class="primary submenu-item" :class="{ active: route.path === '/tasks' }">Tasks</p>
+                        </router-link>
+                        <router-link to="/tags">
+                            <p class="primary submenu-item" :class="{ active: route.path === '/tags' }">Tags</p>
+                        </router-link>
+                    </div>
+                </div>
+            </div>
             <router-link to="/stats">
                 <div class="menu-item" :class="{ active: route.path === '/stats' }">
                     <span class="material-symbols-outlined svg" @mouseover="stats = !stats" @mouseleave="stats = !stats">bar_chart</span>
@@ -49,15 +69,18 @@
 <script setup>
 import { onMounted, ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import { vOnClickOutside } from '@vueuse/components';
 
 const route = useRoute();
 const router = useRouter();
+const editSubmenu = ref(false);
 const isExpanded = ref(false)
 const isToggled = ref(false)
 
 watch(router.currentRoute, () => {
     if (isExpanded.value) isExpanded.value = false;
     if (isToggled.value) isToggled.value = false;
+    if (editSubmenu.value) editSubmenu.value = false;
   });
 
 // Unmounts expand button from dom if width < 480 (portable)
@@ -75,6 +98,15 @@ function toggleMobileMenu() {
         isToggled.value = true
         isExpanded.value = true
     }
+}
+
+function toggleSubmenu() {
+    editSubmenu.value = true
+    isExpanded.value = true
+}
+
+function closeMenu() {
+    isExpanded.value = false
 }
 </script>
 
@@ -123,13 +155,15 @@ function toggleMobileMenu() {
 
     /* Menu item background + color styles */
     .menu-item:hover p, 
-    .menu-item:hover .svg
+    .menu-item:hover .svg,
+    .submenu-item:hover
     {
         color: var(--text);
         background: var(--accent); 
     }
     .menu-item.active p,
-    .menu-item.active .svg {
+    .menu-item.active .svg,
+    .submenu-item.active {
         color: var(--text);
         background: var(--secondary);
     }
@@ -143,6 +177,21 @@ function toggleMobileMenu() {
         }
     p {
         width: calc(80% - 2%); /* width without the padding of the p to not deform the svg */
+    }
+    /* Submenu */
+    .submenu {
+        display: flex;
+        flex-direction: column;
+    }
+    .submenu-item {
+        align-items: flex-end;
+        text-align: center;
+        font-weight: 500;
+        width: 75%;
+        padding: 0;
+        margin: 0;
+        margin-left:auto;
+        border-radius: 0.3em;
     }
 
     @media screen and (min-width: 764px) {
