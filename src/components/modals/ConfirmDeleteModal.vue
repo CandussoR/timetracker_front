d2e7b55559e32b1e4ff22e969e5355b0a7cf5636
CandussoR <r.candusso@protonmail.com>
2024-03-19 18:36:12 +0100

    feat : pasing task & subtask in View, branching delete method, creating and branching ConfirmDelete modal (events still buged)

---
+++ src/components/modals/ConfirmDeleteModal.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="form-container">
+        <h2>Confirm Deletion</h2>
+
+        <p>Are you sure you want to delete this task ?</p>
+
+        <div class="button-row">
+            <button class="button secondary" @click="$emit('confirm')">Yes</button>
+            <button class="button" @click="$emit('cancel')">Cancel</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
--- src/components/modals/ModalFrame.vue
+++ src/components/modals/ModalFrame.vue
@@ -1,13 +1,15 @@
 <template>
     <div id="modal" class="modal">
         <div class="modalContent" v-if="content == 'newTask'"><NewTaskModal/></div>
-        <div class="modalContent" v-if="content == 'newTag'"><NewTagModal/></div>
+        <div class="modalContent" v-else-if="content == 'newTag'"><NewTagModal/></div>
+        <div class="modalContent" v-else-if="content == 'confirmDelete'" @confirm="$emit('confirm')" @cancel="$emit('cancel')"><ConfirmDeleteModal/></div>
     </div>
 </template>
 
 <script setup>
 import NewTaskModal from './NewTaskModal.vue';
 import NewTagModal from './NewTagModal.vue';
+import ConfirmDeleteModal from './ConfirmDeleteModal.vue';
 
 defineProps({
     content : String
