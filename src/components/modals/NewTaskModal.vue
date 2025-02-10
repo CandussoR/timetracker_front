024474cbbfc28c60bf220a3d2a5ed42d170ecf81
Romain Candusso <r.candusso@protonmail.com>
2025-02-10 15:22:12 +0100

    feat : improving Task and Tag modals
    This commit add a new error code management in the Task and Tag modals,
    taking into account a 422 return from the API in case of an already
    existing ressource.
    It also adds a validation for spaces and displays a new Info component
    in case one is found, and some tweks on the spacing of UI elements.

--- src/components/modals/NewTaskModal.vue
+++ src/components/modals/NewTaskModal.vue
@@ -2,6 +2,7 @@
     <h3>New Task</h3>
     <div if="form-container" class="form-container">
         <form ref="createTaskForm" @submit.prevent="handleSubmit">
+            <Info v-if="displayInfo" msg="Task or subtask must not contain any space." />
             <div class="form-input">
                 <label for="newtaskname" hidden="hidden">Task Name</label>
                 <input id="newtaskname" type="text" placeholder="Task Name" :maxlength="30" v-model="taskname" required/>
@@ -10,38 +11,45 @@
                 <label for="newsubtask" hidden="hidden">Subtask</label>
                 <input id="newsubtask" type="text" placeholder="Subtask (optional)" :maxlength="30" v-model="subtask" />
             </div>
-            <button class="button" type="submit">Create</button> 
             <p v-if="successMsg" class="success">{{ successMsg }}</p>
+            <p v-else-if="errorMsg" class="error">{{ errorMsg }}</p>
+            <button class="button" type="submit">Create</button> 
         </form>
     </div>
 
 </template>
 
 <script setup>
+import Info from "@/components/Info.vue";
 import { useTaskStore } from '@/stores/task';
 import { ref } from 'vue';
 
 const taskStore = useTaskStore()
 const taskname = ref(null)
 const subtask = ref(null)
-const successMsg = ref('')
+const successMsg = ref(null)
+const errorMsg = ref(null)
+const displayInfo = ref(false)
 
 async function handleSubmit() {
+    displayInfo.value = false
+    if (taskname.value && taskname.value.includes(' ') || subtask.value && subtask.value.includes(' ')) {
+        displayInfo.value = true;
+        return
+    }
     taskStore.isCreated = false
     const res = await taskStore.createTask(taskname.value, subtask.value)
-    if (res == 200) {
-        successMsg.value = "Task successfully created."
+    if (res.status == 200) {
+        successMsg.value = "Task successfully created !"
+    } else if (res.status == 422) {
+        errorMsg.value = res.data
+    } else {
+        errorMsg.value = "An error occured."
     }
-
 }
 </script>
 
 <style scoped>
-
-.success {
-    color : #80ED99;
-    font-weight: bold;
-}
 input {
     width: 100%;
 }
