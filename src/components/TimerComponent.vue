c66fbd236ac5ed22143479d216604c9357757dec
Romain Candusso <r.candusso@protonmail.com>
2025-03-26 20:06:54 +0100

    refactor : use local storage
    We store the informations in the localStorage to make it easier to retrieve the current timer and prepare the Flow feature, which needs to pass some data from one view to another, easier done with localStorage.

--- src/components/TimerComponent.vue
+++ src/components/TimerComponent.vue
@@ -8,38 +8,36 @@
         </div>
 
         <button v-if="!timerRunning && !stopwatchRunning && !isDone" class="button"
-            @click="currentDuration ? beginTimeRecord('timer') : beginTimeRecord('stopwatch')">
+            @click="handleBeginClick">
             <span class="material-symbols-outlined big"> play_circle </span>
         </button>
         <button v-else-if="(timerRunning || stopwatchRunning) && !isDone" class="button" @click="stopTheClock()">
             <span class="material-symbols-outlined"> stop_circle </span>
         </button>
-        <div id="log-form" v-else>
+        <div id="log-form" v-else-if="!(props.break)">
             <h3>Log</h3>
             <textarea v-model="log"></textarea>
             <button class="button" @click="updateTimeRecord()">Send</button>
             <p id="success" name="success" class="success">{{ success }}</p>
             <div id="update-time">
                 <p>In the end, you kept on going ? Don't worry !</p>
-                <button class="button" @click="console.log('Not implemented yet but I heard that')">Update to
-                    now</button>
+                <update-last-timer-button/>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { useStatStore } from '@/stores/stats';
 import { useTimeRecordStore } from '@/stores/timeRecord';
 import formatTime from '@/utils/formatTime';
 import getCurrentDateTime from '@/utils/getCurrentDateTime';
 import cleanObject from '@/utils/cleanObject';
 import { ref, computed } from 'vue';
-import { useRouter } from 'vue-router';
+import UpdateLastTimerButton from './UpdateLastTimerButton.vue';
 
-const router = useRouter()
+const props = defineProps({break : Boolean})
+const emit = defineEmits(['end', 'launch'])
 const timeRecordStore = useTimeRecordStore()
-const statStore = useStatStore()
 const timeRecord = ref(null)
 const log = ref(null)
 const timerRunning = ref(false)
@@ -47,22 +45,32 @@ const stopwatchRunning = ref(false)
 const isDone = ref(false)
 const currentDuration = ref(0)
 const formattedDuration = computed(() => formatTime(currentDuration.value))
-console.log(formattedDuration)
 const err = ref('')
 const success = ref('')
 
 // Initialising
-timeRecord.value = JSON.parse(sessionStorage.getItem('time_record'))
-// timeRecord.value = JSON.parse(data)
-let data2 = sessionStorage.getItem('duration')
-console.log(data2)
+timeRecord.value = JSON.parse(localStorage.getItem('time_record'))
+let data2 = localStorage.getItem('duration')
 if (data2) {
-    console.log("data2")
     currentDuration.value = Number(data2)
 }
-console.log(formattedDuration)
 
 let worker;
+
+function handleBeginClick() {
+    if (props.break) {
+        const { currentDate, currentTime } = getCurrentDateTime()
+        const datetime = new Date(currentDate + ' ' + currentTime)
+        startTheClock("timer", datetime)
+        return;
+    }
+
+    if (currentDuration.value) {
+        beginTimeRecord('timer')
+    } else {
+        beginTimeRecord('stopwatch')
+    }
+}
 
 function beginTimeRecord(clockType) {
     const { currentDate, currentTime } = getCurrentDateTime()
@@ -76,10 +84,11 @@ function beginTimeRecord(clockType) {
     timeRecordStore.createTimeRecord(tr, "begin")
         .then((res) => {
             // puts it in the storage in case of refresh
-            sessionStorage.setItem("time_record", JSON.stringify(res))
+            localStorage.setItem("time_record", JSON.stringify(res))
             // puts it in the timeRecord for convenience
             timeRecord.value = res
 
+            emit('launch')
             if (clockType == "timer") {
                 startTheClock("timer", datetime)
             } else {
@@ -125,6 +134,9 @@ function stopTheClock() {
     isDone.value = true
     const audio = new Audio(import.meta.env.VITE_APP_RING)
     audio.play()
+    if (props.break) {
+        emit('end', null)
+    }
 }
 
 
@@ -132,15 +144,7 @@ function updateTimeRecord() {
     const tr = {"guid" : timeRecord.value["guid"],
                 "time_ending" : timeRecord.value["time_ending"],
                 "log" : log.value}
-    timeRecordStore.updateTimeRecord(tr, "ending")
-        .then(() => {
-            success.value = "Your timer has been saved. You will now be redirected."
-            sessionStorage.removeItem('time_record')
-            sessionStorage.removeItem('duration')
-        })
-        .catch((e) => err.value = e)
-    statStore.handleUpdated()
-    setTimeout(function() { router.push({ path: '/'})}, 2000)
+    emit('end', tr)
 }
 </script>
 
--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -1,35 +1,113 @@
 <template>
-    <main id="clock">
-        <h1>{{timeRecordStore.displayOngoingInfos.clock || 'Timer'}}</h1>
+    <main id="clock" v-if="displayInfo">
+        <h1 >{{displayInfo.title}}</h1>
+        <h2 v-if="flow.current.type == 'timer'">Timer {{flow.current.i + 1}} / {{flow.infos.f.length}}</h2>
+        <h2 v-else>Break</h2>
 
-        <div id="record__info" v-if="timeRecordStore.displayOngoingInfos">
-            <p id="record__info-task" class="task" v-if="timeRecordStore.displayOngoingInfos.task_name">{{timeRecordStore.displayOngoingInfos.task_name}}
-                <span id="record__info-subtask" v-if="timeRecordStore.displayOngoingInfos.subtask">({{timeRecordStore.displayOngoingInfos.subtask}})</span>
+        <div id="record__info" class="button-row" v-if="displayInfo && displayInfo.title != 'Break'">
+            <p id="record__info-task" class="task" v-if="displayInfo.task_name">{{displayInfo.task_name}}
+                <span id="record__info-subtask" v-if="displayInfo.subtask">({{displayInfo.subtask}})</span>
             </p>
-            <p id="record__info-tag" class="tag" v-if="timeRecordStore.displayOngoingInfos.tag">{{timeRecordStore.displayOngoingInfos.tag}}</p>
+            <p id="record__info-tag" class="tag" v-if="displayInfo.tag">{{displayInfo.tag}}</p>
+            <span class="material-symbols-outlined icon-clear-gradient" @click="handleEdit" title="Edit task & tag">edit</span>
         </div>
 
-        <TimerComponent />
+        <TimerComponent @end="handleUpdateEnd($event)" @launch="hideEdit = !hideEdit" :break="displayInfo.title === 'Break'" :key="displayInfo.title"/>
     </main>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useTimeRecordStore } from '../stores/timeRecord.js';
 import { useRouter } from 'vue-router';
 import TimerComponent from '@/components/TimerComponent.vue';
+import { useStatStore } from '@/stores/stats.js';
 
 const timeRecordStore = useTimeRecordStore()
+const statStore = useStatStore();
 const router = useRouter()
+const displayInfo = ref(null)
+const hideEdit = ref(false)
+
+let flow = localStorage.getItem('flow')
+if (flow) {
+    flow = JSON.parse(flow)
+}
 
 onMounted(() => {
-    if (!timeRecordStore.displayOngoingInfos) {
+    if (flow) {
+        console.log("that's a flow right there we need to do something!")
+    }
+    let di = localStorage.getItem('displayInfos');
+    if (!di) {
         router.push('/new');
     } else {
-        console.log("timeRecordStore")
+        displayInfo.value = JSON.parse(di)
     }
 })
 
+
+function handleEdit() {
+    router.push('/flow/edit');
+}
+
+
+async function handleUpdateEnd(tr) {
+    // If no tr, it's the end of a break.
+    if (!tr && flow) {
+        updateLocalStorage(true)
+        return
+    } else if (!tr && !flow) {
+        localStorage.clear()
+        router.push("/new")
+        return
+    }
+
+    try {
+        await timeRecordStore.updateTimeRecord(tr, "ending")
+        
+        if (!flow) {
+            localStorage.removeItem('time_record')
+            localStorage.removeItem('duration')
+        }
+    } catch (e) {
+        err.value = e
+    }
+
+    statStore.handleUpdated()
+
+    if (flow) {
+        updateLocalStorage(true)
+    } else {
+        success.value = "Your timer has been saved. You will now be redirected."
+        setTimeout(function() { router.push({ path: '/'})}, 1500)
+    }
+}
+
+
+function updateLocalStorage(is_flow) {
+    if (!is_flow) {
+        localStorage.clear()
+        return
+    }
+
+    let tr =JSON.parse(localStorage.getItem('time_record'))
+    console.log("In update local storage with a flow, retrieving the time_record", tr)
+    tr.date = null
+    tr.guid = null
+    localStorage.setItem('time_record', JSON.stringify(tr))
+
+    let wasTimer = flow.current.type == "timer"
+    flow.current = {"type" : flow.current.type == "timer" ? "break" : "timer", "i": flow.current.i+1}
+    localStorage.setItem('flow', JSON.stringify(flow))
+    displayInfo.value.title = wasTimer ? "Break" : "Timer"
+
+    localStorage.setItem("duration", flow.info.f[flow.current.i][wasTimer ? "b" : "t"])
+
+    if (!wasTimer) {
+        hideEdit.value = false
+    }
+}
 </script>
 
 <style scoped>
@@ -38,8 +116,6 @@ main {
     flex-direction : column;
     place-items : center;
 }
-
-
 
 @media screen and (max-height : 300px) {
     h1, p.task {
