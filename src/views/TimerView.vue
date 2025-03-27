56ee48e6f603ec35aadf4288544afe3a8c511f3c
Romain Candusso <r.candusso@protonmail.com>
2025-03-27 18:39:18 +0100

    feat : adding management of flows
    We can now select a flow and move on to the steps of defining the timers for the flow.

--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -1,7 +1,7 @@
 <template>
     <main id="clock" v-if="displayInfo">
         <h1 >{{displayInfo.title}}</h1>
-        <h2 v-if="flow.current.type == 'timer'">Timer {{flow.current.i + 1}} / {{flow.infos.f.length}}</h2>
+        <h2 v-if="flow && flow.current.type == 'timer'">Timer {{flow.current.i + 1}} / {{flow.infos.f.length}}</h2>
         <h2 v-else>Break</h2>
 
         <div id="record__info" class="button-row" v-if="displayInfo && displayInfo.title != 'Break'">
@@ -12,22 +12,45 @@
             <span class="material-symbols-outlined icon-clear-gradient" @click="handleEdit" title="Edit task & tag">edit</span>
         </div>
 
-        <TimerComponent @end="handleUpdateEnd($event)" @launch="hideEdit = !hideEdit" :break="displayInfo.title === 'Break'" :key="displayInfo.title"/>
+        <TimerComponent @end="handleUpdateEnd($event)" @launch="handleLaunch" @stopped="isRunning = false" :send="sendTimerEnd" :break="displayInfo.title === 'Break'" :key="displayInfo.title"/>
+        <p id="success" name="success" class="success">{{ success }}</p>
+
+        <Overlay v-if="warnBeforeLeave" />
+        <div class="modal" v-if="warnBeforeLeave">
+            <div>
+                <p v-if="isRunning">A timer is currently running : stop it before you leave.</p>
+                <p v-else-if="flow">Quitting this page will stop the current flow.</p>
+                <div v-if="isRunning" class="button-row">
+                    <button v-if="isRunning" class="button" @click="handleQuit('ok')">Ok</button>
+                </div>
+                <div v-else class="button-row">
+                    <button class="button" @click="handleQuit('leave')">Leave</button>
+                    <button  class="button" @click="handleQuit('cancel')">Cancel</button>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import { useTimeRecordStore } from '../stores/timeRecord.js';
-import { useRouter } from 'vue-router';
+import { onBeforeRouteLeave, useRouter } from 'vue-router';
 import TimerComponent from '@/components/TimerComponent.vue';
 import { useStatStore } from '@/stores/stats.js';
+import Overlay from '@/components/Overlay.vue';
 
 const timeRecordStore = useTimeRecordStore()
 const statStore = useStatStore();
 const router = useRouter()
 const displayInfo = ref(null)
+const isRunning = ref(false)
 const hideEdit = ref(false)
+const warnBeforeLeave = ref(false)
+const receivedEnd = ref(true)
+const leaveTo = ref(null)
+const sendTimerEnd = ref(false)
+const success = ref(null)
 
 let flow = localStorage.getItem('flow')
 if (flow) {
@@ -35,9 +58,6 @@ if (flow) {
 }
 
 onMounted(() => {
-    if (flow) {
-        console.log("that's a flow right there we need to do something!")
-    }
     let di = localStorage.getItem('displayInfos');
     if (!di) {
         router.push('/new');
@@ -46,13 +66,52 @@ onMounted(() => {
     }
 })
 
+// Navigation guard
+onBeforeRouteLeave((to, from) => {
+    // Authorize editing of task & tag in flow
+    if (!isRunning.value && flow && receivedEnd.value && ['/flow', '/flow/edit'].includes(to.path)) {
+        return true
+    } else if (!isRunning.value && flow && receivedEnd.value && !['/flow', '/flow/edit'].includes(to.path)) {
+        localStorage.clear()
+        return true
+    }
+    // Authorize leaving if nothing has been done
+    if (!isRunning.value && receivedEnd.value) {
+        localStorage.clear()
+        return true
+    }
+    // should I stay or should I go now ?
+    if (isRunning.value && !(warnBeforeLeave.value)) {
+        leaveTo.value = to.path;
+        warnBeforeLeave.value = true;
+        return false
+    }
+
+    if (!isRunning.value && !receivedEnd.value) {
+        sendTimerEnd.value = true
+    }
+
+    if (to.path != '/flow' || to.path != '/flow/edit') {
+        localStorage.clear()
+    }
+})
 
 function handleEdit() {
     router.push('/flow/edit');
 }
 
 
+function handleLaunch() {
+    hideEdit.value = true;
+    isRunning.value = true;
+    receivedEnd.value = false;
+}
+
+
 async function handleUpdateEnd(tr) {
+    isRunning.value = false
+    receivedEnd.value = true
+
     // If no tr, it's the end of a break.
     if (!tr && flow) {
         updateLocalStorage(true)
@@ -78,7 +137,7 @@ async function handleUpdateEnd(tr) {
 
     if (flow) {
         updateLocalStorage(true)
-    } else {
+    } else if (!sendTimerEnd.value) {
         success.value = "Your timer has been saved. You will now be redirected."
         setTimeout(function() { router.push({ path: '/'})}, 1500)
     }
@@ -92,7 +151,6 @@ function updateLocalStorage(is_flow) {
     }
 
     let tr =JSON.parse(localStorage.getItem('time_record'))
-    console.log("In update local storage with a flow, retrieving the time_record", tr)
     tr.date = null
     tr.guid = null
     localStorage.setItem('time_record', JSON.stringify(tr))
@@ -106,6 +164,14 @@ function updateLocalStorage(is_flow) {
 
     if (!wasTimer) {
         hideEdit.value = false
+    }
+}
+
+function handleQuit(answer) {
+    if (answer == 'leave') {
+        router.push(leaveTo)
+    } else {
+        warnBeforeLeave.value = false
     }
 }
 </script>
