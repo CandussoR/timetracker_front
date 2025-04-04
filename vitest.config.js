e0a72428647719c1f3ac4815203ce79c3d0c651d
Romain Candusso <r.candusso@protonmail.com>
2025-04-04 21:20:48 +0200

    fix : play audio file from tauri

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base:"./",
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
