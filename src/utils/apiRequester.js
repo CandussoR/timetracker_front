797d5c62bdc00d110b036a0d82b37b06b8c95914
Romain Candusso <r.candusso@protonmail.com>
2025-04-09 22:06:20 +0200

    feat : different port whether dev or prod

--- src/utils/apiRequester.js
+++ src/utils/apiRequester.js
@@ -2,6 +2,8 @@
 
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_IP_DEV
+axios.defaults.baseURL = import.meta.env.DEV
+    ? import.meta.env.VITE_APP_IP_DEV
+    : import.meta.env.VITE_APP_IP_PROD
 
 export default axios
