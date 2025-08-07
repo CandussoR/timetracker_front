24a151351ea29f8cf71233d70549b72022947b1d
Romain Candusso <r.candusso@protonmail.com>
2025-08-07 15:35:24 +0200

    fix : typo

--- src/utils/getMinDate.js
+++ src/utils/getMinDate.js
@@ -5,7 +5,7 @@ async function getMinDate() {
         const res = await axios.get('/time_records/first_record_date')
         return new Date(res.data)
     } catch(e) {
-        error(`Error while fetching first date : ${e}`);
+        console.error(`Error while fetching first date : ${e}`);
     }
 }
 
